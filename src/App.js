766e216a97b9459688035291b19af19288e9e092
thajunnisaph <thajunnisaph123@gmail.com>
2022-12-25 15:59:51 +0530

    Login page and home page created,also save the token to localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { Fragment } from "react";
+import { Route, Switch,Redirect} from "react-router-dom";
 import Header from "./Components/Layout/Header";
 import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <Fragment>
       <header>
         <Header />
+        <Switch>
+        <Route  path='/login'>
         <Login />
+        </Route>
+        <Route path='/home'>
+          <Home />
+        </Route>
+        <Route path = '/' exact>
+          <Redirect to='/login'/>
+        </Route>
+        </Switch>
+        
         </header>
       
     </Fragment>
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Home.module.css';
+function Home() {
+  return (
+    <div className={classes.home}>
+        <h1>Welcome To Expense Tracker</h1>
+    </div>
+  )
+}
+
+export default Home
