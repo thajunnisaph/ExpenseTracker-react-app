dbf89e7dde26828e8020e7999c2048dd1e7e5e80
thajunnisaph <thajunnisaph123@gmail.com>
2022-12-25 19:58:18 +0530

    use details edit functionality added- fetch previous details from firebase and update

--- src/Pages/UpdateProfile.js
+++ src/Pages/UpdateProfile.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 import React, { useRef } from "react";
+import { useEffect } from "react";
 import classes from "./UpdateProfile.module.css";
 
 function UpdateProfile() {
   const nameref = useRef();
   const urlref = useRef();
+  useEffect(() =>{
+  const token = localStorage.getItem('token');
+  if(token){
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCCSFqdQN1EWYLkEJ3bEpkLcrhbKfMsKO8',{
+        idToken:token
+    }).then((res) =>{
+        console.log(res.data);
+        nameref.current.value = res.data.users[0].displayName;
+        urlref.current.value = res.data.users[0].photoUrl;
+    }).catch((err) =>{
+        console.log(err)
+    })
+  }
+  },[]);
   async function updateHandler (e)  {
     e.preventDefault();
     const name = nameref.current.value;
