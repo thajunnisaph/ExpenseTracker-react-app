d91bae027e3cd46012c50f79fc362246d41e1231
thajunnisaph <thajunnisaph123@gmail.com>
2022-12-25 22:56:59 +0530

    logout function added

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import classes from './Header.module.css';
 
 function Header() {
+  const history = useHistory();
+  const logoutHandler = () =>{
+    localStorage.removeItem('token');
+     history.replace('/login');
+  }
   return (
     <div className={classes.header}>
         <h1>Expense Tracker</h1>
+        <div className={classes.logout}>
+        <button onClick={logoutHandler}>Logout</button></div>
     </div>
   )
 }
